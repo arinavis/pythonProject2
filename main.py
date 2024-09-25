dc9a280af2211d43bff83dda291a86620380a09b
arinavis <avisnevskaja@gmail.com>
2024-09-25 20:01:27 +0300

    3 uzduotis pavyko

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import statistics
 
 # Kintamieji ir sąlygos 2024-09-25
 
@@ -11,14 +12,32 @@ import random
 # veikia
 
 # 2 uzd
-kint1 = random.randint(0, 4)
-kint2 = random.randint(0, 4)
+# kint1 = random.randint(0, 4)
+# kint2 = random.randint(0, 4)
 
-print(kint1)
-print(kint2)
-
-if kint1 != 0 and kint2 != 0:
-    if kint1 > kint2: print(int(kint1 / kint2))
-    else: print(int(kint2 / kint1))
-else: print('Dalyba iš 0 negalima')
+# print(kint1)
+# print(kint2)
+#
+# if kint1 != 0 and kint2 != 0:
+#     if kint1 > kint2: print(int(kint1 / kint2))
+#     else: print(int(kint2 / kint1))
+# else: print('Dalyba iš 0 negalima')
 # veikia
+
+# 3 uzd.
+
+randSk1 = random.randint(0, 25)
+randSk2 = random.randint(0, 25)
+randSk3 = random.randint(0, 25)
+
+print(randSk1)
+print(randSk2)
+print(randSk3)
+
+# print(statistics.median([randSk1, randSk2, randSk3]))
+
+if (randSk1 > randSk2 and randSk1 < randSk3) or (randSk1 > randSk3 and randSk1 < randSk2):
+    print("skaičius 1")
+if (randSk2 > randSk1 and randSk2 < randSk3) or (randSk2 > randSk3 and randSk2 < randSk1):
+    print('Skaičius 2')
+else: print('Skaičius 3')
