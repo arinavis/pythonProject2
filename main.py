04b460c8b30e5a7ee110b6d5e7bd418f4eaf2730
arinavis <avisnevskaja@gmail.com>
2024-09-25 18:54:52 +0300

    1 užduotis pavyko

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 
 # Kintamieji ir sąlygos 2024-09-25
+vardas = str( input('Mano vardas: '))
+pavardė = str( input('Mano pavardė: '))
+gimMetai = int( input('Mano gimimo metai: '))
+todayY= int( input('Šie metai: '))
+print(f'Jūsų amžius: {todayY - gimMetai}')
+# veikia
+
